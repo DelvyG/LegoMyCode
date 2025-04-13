933bc6a3bf2ca7714afa7bd74a83a1d806da50b6
Delvy González <digitalgroup21@gmail.com>
2025-04-13 11:10:59 -0400

    fix vite.congig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
 
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: resolve(new URL('.', import.meta.url).pathname, 'index.html'),
-  },
-},
-});
+      
+const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    export default defineConfig({
+      build: {
+        outDir: 'dist',
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+          },
+        },
+      },
+      publicDir: 'public',
+    });
