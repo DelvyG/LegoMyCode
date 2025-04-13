61545663c051093f1206a2ac241cb5071d01ee07
Delvy González <digitalgroup21@gmail.com>
2025-04-13 04:30:51 -0400

    fix _headers

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'), // <-- ***IMPORTANTE: apunta a index.html***
-      },
-    },
+      input: resolve(new URL('.', import.meta.url).pathname, 'index.html'),
   },
-  publicDir: 'public',
+},
 });
