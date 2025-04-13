823ab7dba1ef958b9c7c72bc3c7d26c13695caff
Delvy González <digitalgroup21@gmail.com>
2025-04-13 17:19:24 -0400

    Agregar carpeta dist al repositorio

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  // Punto de entrada HTML (ubicado en la raíz)
-  root: '.',  
   build: {
-    // Donde se generarán los archivos de build
     outDir: 'dist',
-    rollupOptions: {
-      // Punto de entrada JS (ubicado en src/)
-      input: path.resolve(__dirname, './src/main.js'), 
-    },
+    emptyOutDir: true,  // ← ¡Importante! Limpia el directorio en cada build
   },
+  publicDir: 'public'  // Carpeta para assets estáticos
 });
